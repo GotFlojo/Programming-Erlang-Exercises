04988ffea679b2e0d2dad64d37afb5650a7fc9e1
Florian Fischer <florian-fischer@linguwerk.de>
2015-03-14 22:07:05 +0100

    Exercise 4.2 -> process circle
    Program that creates N processes connected in a ring, where
    the last process spawned is connected to the first process.
    Once started the processes will send M messages around the ring.
    They will terminate gracefully once they receive a quit message.

---
+++ Ex4/processcircle.erl
@@ -0,0 +1,29 @@
+-module(processcircle).
+-export([start/3]).
+
+start(N, M, Message) ->
+    Pid = spawn(processcircle, init, [N-1, self()]),
+    Pid ! {self(), Message, M},
+    loop(Pid),
+    ok.
+
+init(1, Pid) ->
+    loop(Pid);
+init(N, Pid) ->
+    Child = spawn(processcircle, init, [N-1, Pid]),
+    loop(Child).
+
+loop(Pid) ->
+    receive
+        {_, _, 0} -> 
+            %io:format("Final from ~p~n", [From]), 
+            loop(Pid);
+        {From, Message, Counter} ->
+            io:format("From ~p ~p~n", [From, Message]),
+            Pid ! {self(), Message, Counter-1},
+            loop(Pid);
+        quit -> 
+            Pid ! quit,
+            io:format("Quitting ~p~n", [self()]), 
+            true
+    end.
